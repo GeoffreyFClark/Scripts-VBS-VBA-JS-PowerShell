6fd7edfcd49b2b9845f7b758b8b2986d2f6e7056
Geoffrey F. Clark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-10-28 16:44:18 -0600

    very important update

--- abort_shutdown.ps1
+++ abort_shutdown.ps1
@@ -1,3 +1,3 @@
-# For the government workstations that boot up more slowly than progress in Congressional debate.
-# We see your sudden "Your computer will restart in 1 minute" and raise you a:
+# For government workstations that boot up more slowly than bipartisan progress in Congress...
+# We see your sudden "Computer will restart in 1 minute" warning and raise you a:
 shutdown -a
